da1ab383b3bd1fed736cb6c1c04874970d523585
Nekilc <nieaowei@gmail.com>
2022-12-15 04:45:31 +0800

    feat: sanitizeFileName

--- docs/assets/Application-ce8ce5a4.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./_plugin-vue_export-helper-c27b6911.js";import{o,c as e}from"./index-a39eb738.js";const n={};function t(r,a){return o(),e("span",null,"Application")}const _=c(n,[["render",t]]);export{_ as default};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        ElementPlus()
+    ],
     build: {
-        outDir: 'docs'
-    }
+        outDir: 'docs',
+        rollupOptions: {
+            output: {
+                sanitizeFileName: (fileName) => {
+                    const match = DRIVE_LETTER_REGEX.exec(fileName);
+                    const driveLetter = match ? match[0] : "";
+                    return (
+                        driveLetter +
+                        fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+                    );
+                },
+            },
+        },
+    },
 })
